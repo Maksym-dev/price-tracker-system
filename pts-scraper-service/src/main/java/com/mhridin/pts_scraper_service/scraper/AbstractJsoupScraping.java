a806cb670fe361c0464bafe3b9a7ff3b032f5dc0
Maksym-dev <magridin1995@gmail.com>
2026-03-23 17:36:23 +0200

    [PTS-13] Implement service for choosing price selector dynamically by domain in Scraper Service

---
+++ pts-scraper-service/src/main/java/com/mhridin/pts_scraper_service/scraper/AbstractJsoupScraping.java
@@ -0,0 +1,38 @@
+package com.mhridin.pts_scraper_service.scraper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Slf4j
+public abstract class AbstractJsoupScraping implements ScrapingStrategy {
+
+    private final String domain;
+
+    @Override
+    public BigDecimal fetchPrice(String url) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+                    .timeout(5000)
+                    .get();
+
+            return extractPriceFromDocument(doc);
+        } catch (Exception e) {
+            log.error("Error scraping Jsoup site {}: {}", url, e.getMessage());
+            return new BigDecimal(0);
+        }
+    }
+
+    @Override
+    public boolean supports(String url) {
+        return url.contains(domain);
+    }
+
+    protected abstract BigDecimal extractPriceFromDocument(Document doc);
+}
