6bae3c1099572d37b823f53da6776fed5699cbff
Maksym-dev <magridin1995@gmail.com>
2026-03-26 16:55:47 +0200

    [PTS-20] Implement cron job in Scheduler Service for sending message to Kafka for product price updating

--- pts-scheduler-service/src/main/java/com/mhridin/pts_scheduler_service/PtsSchedulerServiceApplication.java
+++ pts-scheduler-service/src/main/java/com/mhridin/pts_scheduler_service/PtsSchedulerServiceApplication.java
@@ -2,8 +2,12 @@ package com.mhridin.pts_scheduler_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.mhridin.pts_common.repository")
+@EntityScan(basePackages = "com.mhridin.pts_common.entity")
 public class PtsSchedulerServiceApplication {
 
 	public static void main(String[] args) {
